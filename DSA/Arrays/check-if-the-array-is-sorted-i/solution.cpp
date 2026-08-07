ce90764722ae184bd61ad531e4d01bded1870198
Ande Keerthan <83347334+Keerthan2047@users.noreply.github.com>
2026-08-07 16:07:23 +0530

    Add solution for Check if the Array is Sorted I - TUFHub

---
+++ DSA/Arrays/check-if-the-array-is-sorted-i/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool arraySortedOrNot(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (arr[i] > arr[j]) return false;
+            }
+        }
+        return true;
+    }
+};
