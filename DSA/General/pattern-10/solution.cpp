98d7f9faad1224fedd60b029584b3269ce1525fe
Ande Keerthan <83347334+Keerthan2047@users.noreply.github.com>
2026-08-07 15:57:22 +0530

    Add solution for Pattern 10 - TUFHub

---
+++ DSA/General/pattern-10/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    void pattern2(int n) {
+        for(int i=0; i < n; i++) {
+    		for(int j=0; j <= i; j++) {
+    			cout << "*";
+			}
+			cout << endl;
+		}
+
+    }
+
+    void pattern5(int n) {
+        for (int i = 1; i <= n; i++) {
+    		for (int j = 1; j <= n-i+1; j++) {
+    			cout << "*";
+			}
+			cout << "\n";
+		}
+    }
+public:
+    void pattern10(int n) {
+        pattern2(n);
+        pattern5(n-1);
+    }
+};
