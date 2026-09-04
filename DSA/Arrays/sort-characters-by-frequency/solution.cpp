06388532c31870f0f82eef71e7279589032f8be5
Ande Keerthan <83347334+Keerthan2047@users.noreply.github.com>
2026-09-04 15:24:06 +0530

    Sync Sort Characters by Frequency [DSA] - TUFHub

--- DSA/Arrays/sort-characters-by-frequency/solution.cpp
+++ DSA/Arrays/sort-characters-by-frequency/solution.cpp
@@ -1,19 +1,22 @@
 class Solution{	
 	public:
 		vector<char> frequencySort(string& s){
-			map<char, int> freq;
+			unordered_map<char, int> freq;
 
+        // Count frequency
         for (char ch : s) {
             freq[ch]++;
         }
 
+        // Store unique characters
         vector<char> ans;
 
         for (auto it : freq) {
             ans.push_back(it.first);
         }
 
-        sort(ans.begin(), ans.end(), [&freq](char a, char b) {
+        // Sort
+        sort(ans.begin(), ans.end(), [&](char a, char b) {
 
             if (freq[a] != freq[b]) {
                 return freq[a] > freq[b];
